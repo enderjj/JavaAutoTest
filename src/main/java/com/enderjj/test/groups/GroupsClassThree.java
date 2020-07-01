e8b0e32a6507c1d80be247e9f7d43ab33d28504d
enderjj <chnhongjj@163.com>
2020-07-01 09:45:14 +0800

    feat: class groups test

--- src/main/java/com/enderjj/test/groups/GroupsClassThree.java
+++ src/main/java/com/enderjj/test/groups/GroupsClassThree.java
@@ -1,4 +1,15 @@
 package com.enderjj.test.groups;
 
+import org.testng.annotations.Test;
+
+@Test(groups = "groupTwo")
 public class GroupsClassThree {
+
+    public void test1() {
+        System.out.println("=== 这是 GroupsClassThree 的 test1 用例 ===");
+    }
+
+    public void test2() {
+        System.out.println("=== 这是 GroupsClassThree 的 test2 用例 ===");
+    }
 }
--- src/main/java/com/enderjj/test/groups/GroupsClassTwo.java
+++ src/main/java/com/enderjj/test/groups/GroupsClassTwo.java
@@ -1,4 +1,15 @@
 package com.enderjj.test.groups;
 
+import org.testng.annotations.Test;
+
+@Test(groups = "groupOne")
 public class GroupsClassTwo {
+
+    public void test1() {
+        System.out.println("=== 这是 GroupsClassTwo 的 test1 用例 ===");
+    }
+
+    public void test2() {
+        System.out.println("=== 这是 GroupsClassTwo 的 test2 用例 ===");
+    }
 }
